9cd713d8894df2c809fd7d584f3dc8bd0cc131b6
jmh <javier.molina@p1labs.mx>
2018-09-30 18:07:05 -0500

    The tests for T are added

--- tests.py
+++ tests.py
@@ -20,6 +20,31 @@ class ContainersTestCase(unittest.TestCase):
             stacks = LargestPalindromeProduct().run()
         self.assertEqual(stacks, expected_stacks)
 
+    def test_obtain_T_by_input(self):
+        T_isnt_a_digit_value = "50a"
+        T_more_than_permited = 101
+        T_less_than_permited = 0
+        T_in_a_correct_range = 50
+
+        with self.assertRaises(ValueError):
+            LargestPalindromeProduct().constraint_for_number_of_test_cases(
+                T_isnt_a_digit_value
+            )
+        with self.assertRaises(ValueError):
+            LargestPalindromeProduct().constraint_for_number_of_test_cases(
+                T_more_than_permited
+            )
+        with self.assertRaises(ValueError):
+            LargestPalindromeProduct().constraint_for_number_of_test_cases(
+                T_less_than_permited
+            )
+        self.assertIsNone(
+            LargestPalindromeProduct().constraint_for_number_of_test_cases(
+                T_in_a_correct_range
+            ),
+            None
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
